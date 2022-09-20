a5cedabccc58a2856137ddc02abb72482aafe723
Ayush <200104019.hbtu.ac.in>
2022-09-20 19:58:08 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made by
+       
+          Ayush
+        
+      </div>
+    );
+  };
+  
+  export default Footer;
